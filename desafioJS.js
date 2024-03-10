47ca2b52acc1a5607ecbfda41935c4c3a1b471b4
atos mendes <atosmendes48@gmail.com>
2024-03-10 17:07:40 -0300

    Desafio dev junior

---
+++ desafioJS.js
@@ -0,0 +1,34 @@
+function procurarUsuario(usuarios, valor) {
+    return new Promise((resolve, reject) => {
+        valor = valor.toLowerCase();
+        for (let usuario of usuarios) {
+            const nome = usuario.nome.toLowerCase();
+            const email = usuario.email.toLowerCase();
+            if (nome === valor || email === valor) {
+                resolve(usuario);
+                return;
+            }
+        }
+        reject("Nenhum usuário encontrado com o valor fornecido.");
+    });
+}
+
+let usuarios = [
+    { nome: 'Atos', idade: 22, email: 'atos@exemplo.com' },
+    { nome: 'Geovanna', idade: 19, email: 'geovanna@exemplo.com' },
+    { nome: 'Neto', idade: 62, email: 'neto@exemplo.com' }
+];
+
+let valorPesquisa = prompt('Digite o valor da pesquisa:');
+
+if (valorPesquisa) {
+    procurarUsuario(usuarios, valorPesquisa)
+        .then(usuario => {
+            console.log('Usuário encontrado:', usuario);
+        })
+        .catch(erro => {
+            console.error('Erro:', erro);
+        });
+} else {
+    console.error('Nenhum valor de pesquisa fornecido.');
+}
